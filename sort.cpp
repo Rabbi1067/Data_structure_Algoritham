104000af0bef61c89e25fb7b224e7b1231b27b4b
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-02-08 13:21:37 +0600

    sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,9 +1,15 @@
-/*2. Sort the array in descending order.
+/*
+1. Take an array of N numbers as input.
+2. Sort the array in descending order.
 (Take the last digit of your student id and mod it by
- 2. if the result=0, use Selection Sort ; if the result=1, use Bubble Sort ;
-  Otherwise, use Insertion Sort)
+ 3. if the result=0, use Selection Sort ; if the result=1,
+  use Bubble Sort ; Otherwise, use Insertion Sort)
+3. Now, search for a key value which will be taken as an input. Use the
+   Binary Search algorithm to find the key value in the sorted array. If
+   found, then print “You have found the key value at Index k”, otherwise
+   print “Sorry, Try Again”. This process will continue until you find the
+   key value in the array.
 */
-
 #include<bits/stdc++.h>
 using namespace std;
 void dselection(int ary[],int n)
@@ -70,6 +76,19 @@ void dinsersion(int ary[],int n)
 }
 
 
+int Bsearch(int arry[],int n,int key)
+{
+    int l=0;
+    int h=n-1;
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(arry[mid]== key) return mid;
+        else if(arry[mid]<key) h=mid-1;
+        else l= mid+1;
+    }
+    return -1;
+}
 
 int main()
 {
@@ -96,7 +115,7 @@ int main()
     {
         dselection(ary,n);
     }
-    else if(id%2==1)
+    else if(id%3==1)
     {
         dbubble(ary,n);
     }
@@ -104,6 +123,22 @@ int main()
     {
         dinsersion(ary,n);
     }
-}
+    int key;
+    while (true)
+    {
+        cout << "Enter the key value to search: ";
+        cin >> key;
 
+        int rabbi = Bsearch(ary, n, key);
+        if (rabbi != -1)
+        {
+            cout << "You have found the key value at Index: " << rabbi << endl;
+            break;
+        }
+        else
+        {
+            cout << "Sorry, Try Again!" << endl;
+        }
+    }
+}
 
