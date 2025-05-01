01c4af2906d4a627449832f18e3b893fb23f02da
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-01 19:42:56 +0600

    Create 0-1 _Knapsack_ Algorithm.cpp

---
+++ 0-1 _Knapsack_ Algorithm.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n) {
+    if (n == 0 || W == 0)
+        return 0;
+
+    int pick = 0;
+    if (wt[n - 1] <= W)
+        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1);
+
+    int notPick = knapsackRec(W, val, wt, n - 1);
+
+    return max(pick, notPick);
+}
+
+int main() {
+    int n, W;
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    vector<int> val(n), wt(n);
+    cout << "Enter values/Profits of items:\n";
+    for (int i = 0; i < n; ++i) cin >> val[i];
+
+    cout << "Enter weights of items:\n";
+    for (int i = 0; i < n; ++i) cin >> wt[i];
+
+    cout << "Enter capacity of knapsack: ";
+    cin >> W;
+
+    cout << "Maximum value in knapsack = " << knapsackRec(W, val, wt, n) << endl;
+    return 0;
+}
