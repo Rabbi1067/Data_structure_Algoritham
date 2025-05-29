86d300a5c2d22860376f8fe7ab877ebb1e783b4a
Fazle Rabbi <fazlerabbicse65@gmail.com>
2025-05-29 08:33:47 +0600

    Update Fractional_bisection_ method _(binary _search).cpp

--- Fractional_bisection_ method _(binary _search).cpp
+++ Fractional_bisection_ method _(binary _search).cpp
@@ -34,3 +34,33 @@ int main() {
 
     return 0;
 }
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to calculate square root using fractional bisection
+double sqrt_bisection(int n ) {
+    double l = 0, h = n, mid;
+    
+    // If n is less than 1 (like 0.25), we need to set upper bound to 1
+    if (n < 1) h = 1;
+
+    while (h - l > 1e-6) {
+        mid = (l + h) / 2;
+        if (mid * mid <= n) {
+            l = mid;
+        } else {
+            h = mid;
+        }
+    }
+
+    return l;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(6) << sqrt_bisection(n) << endl;
+    return 0;
+}
+*/
